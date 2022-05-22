66cd1bad39ce1a4e417cc830a5830732518a54d5
Garfield <13379378389@163.com>
2022-05-22 19:42:38 +0800

    增加按用户类型登录

--- login.php
+++ login.php
@@ -11,6 +11,7 @@ $dbname = "MIS";
 //将用户名和密码存入变量中，供后续使用
 $user = $_POST["admin_name"];
 $psw = $_POST["admin_password"];
+$type=$_POST["admin"];
 if ($user == "" || $psw == "") {
     //用户名或者密码其中之一为空，则弹出对话框，确定后返回当前页的上一页 
     echo "<script>alert('请输入用户名或密码！'); history.go(-1);</script>";
@@ -20,13 +21,27 @@ if ($user == "" || $psw == "") {
         echo mysqli_errno($conn);
         exit;
     }
-
-    $sql = "select username,userpwd from user where username = '$user' and userpwd = '$psw'";
-    $result = mysqli_query($conn, $sql);
-    $num = mysqli_num_rows($result);
-    if ($num) {
-        echo "<script>alert('成功登录'); window.location.href='index.html';</script>";
-    } else {
-        echo "<script>alert('用户名或密码不正确！');history.go(-1);</script>";
+switch($type)
+    {
+        case '0':{ 
+            $sql = "select username,userpwd from user where username = '$user' and userpwd = '$psw'";
+            $result = mysqli_query($conn, $sql);
+            $num = mysqli_num_rows($result);
+            if ($num) {
+                echo "<script>alert('成功登录 '); window.location.href='index.html';</script>";
+                // echo $type;
+            } else {
+                echo "<script>alert('用户名或密码不正确！');history.go(-1);</script>";
+            }
+            break;
+        }
+        case '1':{
+            echo $type;
+            break;
+        }
+        case '2':{
+            echo $type;
+            break;
+        }
     }
 }
